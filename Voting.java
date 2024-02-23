c7fe8af9ac0e19677da0b1df01cb0b461c9cf336
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-02-23 11:40:43 +0530

    Add files via upload

---
+++ Voting.java
@@ -0,0 +1,9 @@
+class Voting
+ {
+   public static void main(String[]args)
+    {
+     int age=23;
+     boolean result =age>=18;
+     System.out.println(result+" - Y our age is eligible for voting");
+    }
+ }
